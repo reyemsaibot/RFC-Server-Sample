8f378feb7c6f9166e23bc8b568ecdd12e52eb1c7
Tobias <reyemsaibot@gmail.com>
2016-03-08 08:50:05 +0100

    Added files via upload

---
+++ NCo_3_RFC_Server_Sample_Tobias_Meyer/NCo_3_RFC_Server_Sample_Tobias_Meyer/Module1.vb
@@ -0,0 +1,12 @@
+﻿Imports SAP.Middleware.Connector
+Imports System.Threading
+
+Module Module1
+
+    Sub Main()
+        Dim RemoteServer As RfcServer
+        RemoteServer = RfcServerManager.GetServer("DINOZZO", New Type(0) {GetType(ServerFunction)})
+        RemoteServer.Start()
+        Thread.Sleep(400000)
+    End Sub
+End Module
